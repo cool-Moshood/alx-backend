a096ace109b13f15449aad7a616d70679788e049
cool-Moshood <moshoodage@gmail.com>
2023-07-27 03:16:38 +0100

    task update

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -13,5 +13,6 @@ class BasicCache(BaseCaching):
 
     def get(self, key):
         """get the cache"""
-        if key is not None and key in self.cache_data:
+        if key is not None and key in self.cache_data.keys():
             return self.cache_data[key]
+        return None
