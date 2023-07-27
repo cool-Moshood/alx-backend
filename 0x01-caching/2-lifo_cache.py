729fb9c6cd869ced6405dacacc3d7330265fe544
cool-Moshood <moshoodage@gmail.com>
2023-07-27 02:52:49 +0100

    task update

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -1,28 +1,42 @@
 #!/usr/bin/env python3
-"""LIFO CACHING"""
-from collections import deque
+""" BaseCaching module
+"""
 from base_caching import BaseCaching
 
 
 class LIFOCache(BaseCaching):
-    """class parent"""
+    """
+    FIFOCache defines a FIFO caching system
+    """
 
     def __init__(self):
+        """
+        Initialize the class with the parent's init method
+        """
         super().__init__()
-        self.key_queue = []
+        self.order = []
 
     def put(self, key, item):
-        """Add an item to cache"""
-        if key is not None and item is not None and key not in self.cache_data:
-            if len(self.cache_data) >= self.MAX_ITEMS:
-                last_item = self.key_queue.pop()
-                del self.cache_data[last_item]
-                print(f"DISCARD: {last_item}")
-        self.cache_data[key] = item
-        self.key_queue.append(key)
+        """
+        Cache a key-value pair
+        """
+        if key is None or item is None:
+            pass
+        else:
+            length = len(self.cache_data)
+            if length >= BaseCaching.MAX_ITEMS and key not in self.cache_data:
+                print("DISCARD: {}".format(self.order[-1]))
+                del self.cache_data[self.order[-1]]
+                del self.order[-1]
+            if key in self.order:
+                del self.order[self.order.index(key)]
+            self.order.append(key)
+            self.cache_data[key] = item
 
     def get(self, key):
-        """get from cache"""
-        if key is None or key not in self.cache_data:
-            return None
-        return self.cache_data[key]
+        """
+        Return the value linked to a given key, or None
+        """
+        if key is not None and key in self.cache_data.keys():
+            return self.cache_data[key]
+        return None
