9e066df27f9b29fb6b83638ce0397ad05a9a6a96
cool-Moshood <moshoodage@gmail.com>
2023-07-27 01:44:11 +0100

    task 2

---
+++ 0x01-caching/2-lifo_cache.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""LIFO CACHING"""
+from base_caching import BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    """class parent"""
+
+    def __init__(self):
+        super().__init__()
+        self.key_queue = []
+
+    def put(self, key, item):
+        """Add an item to cache"""
+        if key is not None and item is not None and key not in self.cache_data:
+            if len(self.cache_data) >= self.MAX_ITEMS:
+                last_item = self.key_queue.pop()
+                del self.cache_data[last_item]
+                print(f"DISCARD: {last_item}")
+        self.cache_data[key] = item
+        self.key_queue.append(key)
+
+    def get(self, key):
+        """get from cache"""
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data[key]
