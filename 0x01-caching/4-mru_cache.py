ace570fbc66123cde05ea3c1cdf790c5f5c95adb
cool-Moshood <moshoodage@gmail.com>
2023-07-27 02:18:04 +0100

    task update

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """Caching module"""
 from collections import deque
-BaseCaching = __import__('base_caching').BaseCaching
+from base_caching import BaseCaching
 
 
 class MRUCache(BaseCaching):
