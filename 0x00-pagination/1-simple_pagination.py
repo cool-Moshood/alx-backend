f56a4ff4ef728955fa3dfdc3ab544af486e02e9a
cool-Moshood <moshoodage@gmail.com>
2023-07-29 01:14:23 +0100

    task 1

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -8,6 +8,8 @@ from typing import List
 
 
 def index_range(page=int, page_size=int) -> tuple:
+    """a function that return page and page_size"""
+
     start_index = ((page - 1) * page_size)
     end_index = start_index + page_size
 
