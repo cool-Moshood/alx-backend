ec3d2e62fe144a8c965ee2be5f161136f0ea39e9
cool-Moshood <moshoodage@gmail.com>
2023-08-02 02:57:10 +0100

    task 3

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 """a flask app"""
 from flask import Flask, render_template, request
-from flask_babel import Babel
+from flask_babel import Babel, gettext as _
+
+app = Flask(__name__)
+Babel = Babel(app)
 
 
 class Config:
@@ -11,20 +14,22 @@ class Config:
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
-app = Flask(__name__)
-Babel = Babel(app)
 app.config.from_object(Config)
 
 
 def get_locale():
-    """determine the best match with our supported languages."""
-    return request.accept_languages.best_match(app.config["LANGUAGES"])
+    """Check if the lang parameter is specified in the query string"""
+    user_lang = request.args.get("lang")
+    if user_lang in app.config["LANGUAGES"]:
+        return user_lang
 
+    """If lang parameter is not specified, use Accept-Language header"""
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
 
 @app.route("/")
 def index():
     """Route function"""
-    return render_template("2-index.html")
+    return render_template("3-index.html")
 
 
 if __name__ == '__main__':
