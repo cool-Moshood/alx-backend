e1d2fa165a144268ec79ae6a5b7c0401cae33808
cool-Moshood <moshoodage@gmail.com>
2023-08-02 04:09:40 +0100

    task update

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -15,7 +15,7 @@ app = Flask(__name__)
 babel = Babel(app)
 app.config.from_object(Config)
 
-
+@babel.localeselector
 def get_locale():
     """determine the best match with our supported languages."""
     return request.accept_languages.best_match(app.config["LANGUAGES"])
